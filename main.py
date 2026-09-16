44070a1f31dd0b164e34819cc63207b2723ede9e
Jonathan Medacier <jonmvirtual@gmail.com>
2026-09-15 22:51:35 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,50 @@
+multiplyone = int(input("Please enter the first number: "))
+multiplytwo = int(input("Please enter the second number: "))
+
+multiplytotal = multiplyone * multiplytwo
+
+print("The answer to your query is:", multiplytotal)
+
+
+divideone = int(input("Please enter the first number: "))
+dividetwo = int(input("Please enter the second number: "))
+
+dividetotal = divideone / dividetwo
+
+print("The answer to your query is:", "%.8f" % dividetotal)
+
+
+noremainderone = int(input("Please enter the first number: "))
+noremaindertwo = int(input("Please enter the second number: "))
+
+noremaindertotal = noremainderone // noremaindertwo
+
+print("The answer to your query is:", noremaindertotal)
+
+
+moduloone = int(input("Please enter the first number: "))
+modulotwo = int(input("Please enter the second number: "))
+
+modulototal = moduloone % modulotwo
+
+print("The answer to your query is:", modulototal)
+
+
+addone = int(input("Please enter the first number: "))
+addtwo = int(input("Please enter the second number: "))
+
+addtotal = addone + addtwo
+
+print("The answer to your query is:", addtotal)
+
+
+subtractone = int(input("Please enter the first number: "))
+subtracttwo = int(input("Please enter the second number: "))
+
+subtracttotal = subtractone - subtracttwo
+
+print("The answer to your query is:", subtracttotal)
+
+
+print("Jonathan" + " " + "Medacier")
+
